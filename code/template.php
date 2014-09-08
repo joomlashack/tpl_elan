7536980ed7f75ef49ee6c9ccf0eeab557a4adede
Roberto <carloscontrerasr@gmail.com>
2014-09-08 11:04:38 +0200

    TPLELAN-17 #comment Not necessary style elements and not used font. According to the design directives this font will not be used on the typo of the page.

--- code/template.php
+++ code/template.php
@@ -17,7 +17,6 @@ defined('_JEXEC') or die('Restricted access');
     <html>
     <head>
         <w:head />
-        <link href='<?php echo JURI::root(true) ?>/templates/js_elan/css/aleo.css' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
     </head>
