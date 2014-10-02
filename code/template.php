a364fe00955008ee9a53f69aeb18f76b3d992c7c
Roberto Contreras <roberto@joomlashack.com>
2014-10-02 19:13:25 +0200

    TPLELAN-21 #comment Flex grid position.

--- code/template.php
+++ code/template.php
@@ -64,7 +64,7 @@
             <?php if ($this->countModules('grid-top')) : ?>
                 <div id="grid-top">
                     <div class="<?php echo $containerClass ?>">
-                        <w:module type="row-fluid" name="grid-top" chrome="wrightflexgrid" extradivs="module" />
+                        <w:module name="grid-top" chrome="wrightflexgrid" extradivs="module" />
                     </div>
                 </div>
             <?php endif; ?>
