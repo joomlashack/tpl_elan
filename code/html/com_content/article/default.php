0dbc8952c96c16c22a470288499e9abf77353efd
jmarkos <markos@joomlashack.com>
2014-10-29 09:26:17 -0600

    Fixing extra div when the item has no image

--- code/html/com_content/article/default.php
+++ code/html/com_content/article/default.php
@@ -3,7 +3,8 @@
  * @package     Wright
  * @subpackage  Overrider
  *
- * @copyright   Copyright (C) 2005 - 2014 Joomlashack. Meritage Assets.  All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2014 Joomlashack. Meritage Assets.
+ * 				All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -11,63 +12,78 @@
 defined('_JEXEC') or die;
 
 $app = JFactory::getApplication();
+$params		= $this->item->params;
+$images = json_decode($this->item->images);
 
-function getFulltextImageFloat($item) {
-    $images = json_decode($item->images);
-    return (empty($images->float_fulltext)) ? $item->params->get('float_fulltext') : $images->float_fulltext;
+if ($params->get('access-view')) {
+	$imageExist = (isset($images->image_fulltext) and !empty($images->image_fulltext)) ? true : false ;
+	$classFloat = 'float-';
+	$classFloat .= (empty($images->float_fulltext)) ? $params->get('float_fulltext') : $images->float_fulltext;
 }
 
-$classFloat = 'float-' . getFulltextImageFloat($this->item);
+if ($imageExist) {
+	switch ($classFloat) {
+		case "float-left":
+		$this->wrightElementsStructure = Array(
+			    "div.row-fluid",
+		        "div.span6" ,
+			        "div.no-pull-image",
+			            'image',
+			        "/div",
+		        "/div",
+		        "div.span6" ,
+		            'icons',
+		            'title',
+		            'content',
+		            'article-info',
+		        "/div",
+		    "/div");
 
-switch ($classFloat) {
-	case "float-left":
-	$this->wrightElementsStructure = Array(
-		    "div.row-fluid",
-	        "div.span6" ,
-		        "div.no-pull-image",
-		            'image',
+		break;
+		case "float-right":
+		$this->wrightElementsStructure = Array(
+			    "div.row-fluid",
+		        "div.span6",
+		        	 'icons',
+		            'title',
+		            'content',
+		            'article-info',
 		        "/div",
-	        "/div",
-	        "div.span6" ,
-	            'icons',
-	            'title',
-	            'content',
-	            'article-info',
-	        "/div",
-	    "/div");
-	 
-	break;
-	case "float-right":
-	$this->wrightElementsStructure = Array(
-		    "div.row-fluid",
-	        "div.span6",
-	        	 'icons',
-	            'title',
-	            'content',
-	            'article-info',
-	        "/div",	
-	        "div.span6" ,
-		        "div.no-pull-image",
-		            'image',
+		        "div.span6" ,
+			        "div.no-pull-image",
+			            'image',
+			        "/div",
 		        "/div",
-	        "/div",
-	    "/div");
-	
-	break;
-	default:
+		    "/div");
+
+		break;
+		default:
+		$this->wrightElementsStructure = Array(
+		    "image",
+		    "div.full-title",
+		    "title",
+		    "icons",
+		   	"/div",
+		    "article-info",
+		    "legendtop",
+		    "content",
+		    "legendbottom"
+		);
+
+		break;
+	}
+}
+else {
 	$this->wrightElementsStructure = Array(
 	    "image",
 	    "div.full-title",
-	    "title",
-	    "icons",
+		    "title",
+		    "icons",
 	   	"/div",
 	    "article-info",
 	    "legendtop",
 	    "content",
-	    "legendbottom"
-	);
- 
-	break;
+	    "legendbottom");
 }
 
 require_once(JPATH_THEMES.'/'.$app->getTemplate().'/'.'wright'.'/'.'html'.'/'.'overrider.php');
