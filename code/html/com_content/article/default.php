21db27755cdc7027dc8fd28909571c8c49871997
Roberto <carloscontrerasr@gmail.com>
2014-10-29 10:17:35 +0100

    TPLELAN-35 #comment Fixing navigation style and position.

--- code/html/com_content/article/default.php
+++ code/html/com_content/article/default.php
@@ -37,6 +37,7 @@ switch ($classFloat) {
 	    "/div");
 	 
 	break;
+	
 	case "float-right":
 	$this->wrightElementsStructure = Array(
 		    "div.row-fluid",
@@ -52,6 +53,21 @@ switch ($classFloat) {
 		        "/div",
 	        "/div",
 	    "/div");
+
+	case "no-pull-image":
+	$this->wrightElementsStructure = Array(
+	    "image",
+	    "div.full-title",
+	    "title",
+	    "icons",
+	   	"/div",
+	    "article-info",
+	    "legendtop",
+	    "content",
+	    "legendbottom"
+	);
+ 
+	break;
 
 	default:
 	$this->wrightElementsStructure = Array(
