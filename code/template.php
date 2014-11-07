3cbeb17126b74a891fa594ece7cbe3332ea5c2c3
jmarkos <markos@joomlashack.com>
2014-11-07 15:15:06 +0100

    Fixing color hover of bottom menu

--- code/template.php
+++ code/template.php
@@ -119,12 +119,12 @@
         </div>
         <div class="wrapper-footer">
             <footer id="footer" <?php if ($this->params->get('stickyFooter',1)) : ?> class="sticky"<?php endif; ?>>
+                <?php if ($this->countModules('bottom-menu')) : ?>
+                <!-- bottom-menu -->
+                <w:nav containerClass="<?php echo $containerClass ?>" rowClass="<?php echo $gridMode; ?>" name="bottom-menu" wrapClass="navbar-transparent" />
+                <!-- bottom-menu end -->
+                <?php endif; ?>
                 <div class="footer-content">
-                    <?php if ($this->countModules('bottom-menu')) : ?>
-                    <!-- bottom-menu -->
-                    <w:nav containerClass="<?php echo $containerClass ?>" rowClass="<?php echo $gridMode; ?>" name="bottom-menu" wrapClass="navbar-transparent" />
-                    <!-- bottom-menu end -->
-                    <?php endif; ?>
                     <?php if ($this->countModules('footer')) : ?>
                     <div class="<?php echo $containerClass ?>">
                         <w:module type="<?php echo $gridMode; ?>" name="footer" chrome="xhtml" />
